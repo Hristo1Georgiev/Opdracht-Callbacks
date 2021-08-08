512732e6590f01ab00bf07722fc8ee25b277458b
Hristo Georgiev <hristo1georgiev@yahoo.com>
2021-08-08 16:55:43 +0200

    Callbacks

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// Opdracht: Callbacks.
+function huiswerkMaken(vak, callback) {
+    setTimeout(() =>{
+    console.log(`${"Ok, ok, ik ga mijn " + `${vak}` + " huiswerk maken."}`);
+}, 3000);
+};
+
+function klaarMetHuiswerk() {
+    console.log(`${"Kijk Paps / Mams, ik ben klaar met mijn huiswerk!"}`)
+}
+
+huiswerkMaken("wiskunde", klaarMetHuiswerk());
+
